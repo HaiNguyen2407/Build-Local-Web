f0c02c70e0f89b522169f9143dd9377721ea9865
HaiNguyen2407 <50288848+HaiNguyen2407@users.noreply.github.com>
2019-05-14 16:37:50 +0700

    Add files via upload

---
+++ Angular.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var demoApp = angular.module('demoApp',[])
+ .controller( "RegisterCtrl", ['$scope', function($scope) {
+ $scope.success = false;
+ $scope.register = function() {
+ $scope.success=true;
+ }
+ }]);
